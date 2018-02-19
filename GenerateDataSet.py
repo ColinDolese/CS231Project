4fd470e9898eab1f8ec07cc3d05e2cd87088f5af
jaagu <jaak.joonas@uudmae.com>
2018-02-19 15:12:04 -0800

    Added code that can generate a png from html or get png binary data directly.

--- GenerateDataSet.py
+++ GenerateDataSet.py
@@ -1,18 +1,24 @@
 from HTMLGenerator import *
-
-
+from HTMLToImage import *
+import os
 
 def getHTMLPages():
-	htmlPages = []
-	for i in range(150):
-		html = generateHTML()
-		page = 'page' + str(i+1) + '.html'
-		f = open(page,"w")
-		f.write(html)
-		f.close()
-		htmlPages.append(page)
-
-	return htmlPages
-
+        htmlPages = []
+        driver = open_driver()  #from HTMLToImage
+        for i in range(10):
+                html = generateHTML()
+                page = 'page' + str(i+1) + '.html'
+                f = open(page,"w")
+                f.write(html)
+                f.close()
+                htmlPages.append(page)
+                #       Generate image from html
+                url = 'file:///'+os.getcwd() +'/' + page
+                url = url.replace('\\', '/')
+                save_html_into_image(driver, url, 'page'+str(i+1)+'.png')
+                #       Or get png data directly
+                #screenshot = get_binary_data_from_url()
+        quit_driver(driver)   #from HTMLToImage
+        return htmlPages
 
 getHTMLPages()
---
+++ HTMLToImage.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+def get_binary_data_from_url(driver, url):
+    driver.get(url)
+    return driver.get_screenshot_as_png()
+    
+def save_html_into_image(driver, url, filename):
+    driver.get(url)
+    screenshot = driver.save_screenshot(filename)
+ 
+
+def open_driver():
+    DRIVER = 'chromedriver'
+    driver = webdriver.Chrome(DRIVER)
+    return driver
+
+def quit_driver(driver):
+    driver.quit()
+    
