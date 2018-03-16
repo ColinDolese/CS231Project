fixed normalization step

GenerateDataSet.py
@@ -2,6 +2,18 @@ from VectorHTMLGenerator import *
 from HTMLToImage import *
 import os
 
+def formatVectorHTML(vectorHTMLEntry, ranges):
+    vecCopy = np.copy(vectorHTMLEntry).astype(float)
+    print(vecCopy)
+    for i in range(vecCopy.shape[0]):
+        vecCopy[i][0] = 0
+        vecCopy[i][6] = 0
+        for j, attr in enumerate(sorted(ranges.iterkeys())):
+                vecCopy[i][j] /= (len(ranges[attr])-1)
+    
+    print(vecCopy)
+    return vecCopy
+
 
 def getHTMLPages(numPagesGen, training, trainpath, testpath):
         htmlPages = []
@@ -16,6 +28,7 @@ def getHTMLPages(numPagesGen, training, trainpath, testpath):
                 vectorHTML = generateVectorHTML(HTMLRanges)
 
                 html = convertToHTML(vectorHTML, HTMLRanges)
+                formatVectorHTML(vectorHTML, HTMLRanges)
                 vectorHTMLs.append(vectorHTML)
                 page = path+'/page' + str(i+1) + '.html'
                 f = open(page,"w")
@@ -35,4 +48,4 @@ def getHTMLPages(numPagesGen, training, trainpath, testpath):
 
 ##getHTMLPages(5)
 
-getHTMLPages(20, True, "test_data/","train_data/")
+getHTMLPages(1, True, "test_data/","train_data/")

learnToGenerateHTML.py
@@ -25,15 +25,17 @@ DATASAMPLES_TRAIN = 100
 
 GENERATE_NEW_TEST = False
 
-def formatVectorHTML(vectorHTMLEntry, ranges):
-    vecCopy = np.copy(vectorHTMLEntry)
-    # set "background" attribute to 0
-    vecCopy[0] = 0
-    # set "order" attribute to 0
-    vecCopy[6] = 0
-    
-    return normalize(vecCopy.reshape(-1,1), axis=0, norm='l1').flatten()
 
+def formatVectorHTML(vectorHTML, ranges):
+    vecCopy = np.copy(vectorHTML).astype(float)
+    print(vecCopy)
+    for i in range(vecCopy.shape[0]):
+        vecCopy[i][0] = 0
+        vecCopy[i][6] = 0
+        for j, attr in enumerate(sorted(ranges.iterkeys())):
+                vecCopy[i][j] /= (len(ranges[attr])-1)
+    
+    return vecCopy
      
 
 def createData(numSamples, training = True):
@@ -71,23 +73,26 @@ def createData(numSamples, training = True):
         
         #2. Match vectorHTMLs with segments
         #   Currently assuming that vec 2-4 are in random order
+
+        normVecHTML = formatVectorHTML(vectorHTMLs[i])
+
         xSamples.append(np.array(segments[0]))
         xSamples.append(np.array(segments[1]))
 
-        ySamples.append(np.array(formatVectorHTML(vectorHTMLs[i][0])))
-        ySamples.append(np.array(formatVectorHTML(vectorHTMLs[i][1])))
+        ySamples.append(np.array(normVecHTML[0]))
+        ySamples.append(np.array(normVecHTML[1]))
         #go over middle vectors that are in random order
         addedVectors = 0
         while addedVectors < 3:
-            for vec in vectorHTMLs[i][2:-1]:
+            for vec in normVecHTML[2:-1]:
                 if vec[-2] == addedVectors + 1:
-                    ySamples.append(np.array(formatVectorHTML(vec)))
+                    ySamples.append(np.array(vec))
                     #-2 index is "order" according to generateVectorHTML
                     xSamples.append(np.array(segments[vec[-2]+1]))
                     addedVectors += 1
             
         xSamples.append(np.array(segments[-1]))
-        ySamples.append(np.array(formatVectorHTML(vectorHTMLs[i][-1])))
+        ySamples.append(np.array(normVecHTML[-1]))
         print ("segment " + str(i) + " done")
 
         i += 1